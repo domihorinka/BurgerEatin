37be50873bbfa21591e9f2d9307cc1af32a28072
domihorinka <domihorinka@gmail.com>
2021-03-01 20:04:19 -0700

    added code to css and views

--- server.js
+++ server.js
@@ -5,3 +5,22 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+const routes = require('./controllers/burgerController.js');
+
+app.use(routes);
+
+
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
